1e23aba04ef313e6b753b0998cc769689b616a24
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-08-01 20:56:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
 from flask import Flask, send_from_directory
-import os
 
-# Flask uygulaması tanımlanıyor
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__)
 
-# Anasayfa
-@app.route('/')
+@app.route("/")
 def home():
-    return '✅ Meli & Melo ses sunucusu aktif!'
+    return "✅ Meli & Melo ses sunucusu aktif!"
 
-# MP3 dosyaları için URL
-@app.route('/static/<path:filename>')
-def serve_static(filename):
-    return send_from_directory(app.static_folder, filename)
+@app.route("/static/<path:filename>")
+def static_files(filename):
+    return send_from_directory("static", filename)
 
-# Uygulama başlatılıyor
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
