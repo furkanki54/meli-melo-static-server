92749dd2d931a16c60703a976be0659f415a03b3
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-08-01 21:01:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
-def index():
+def home():
     return 'Meli & Melo Static Server Aktif!'
 
 @app.route('/static/<path:filename>')
@@ -13,4 +13,4 @@ def serve_static(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
