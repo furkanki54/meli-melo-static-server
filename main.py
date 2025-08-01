9a5dd3b490f383aaead9a4a9618dd7a5031bea5a
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-08-01 20:58:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
-@app.route("/")
-def home():
-    return "✅ Meli & Melo ses sunucusu aktif!"
+@app.route('/')
+def index():
+    return 'Meli & Melo Static Server Aktif!'
 
-@app.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory("static", filename)
+@app.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
