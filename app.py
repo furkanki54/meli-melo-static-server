110bcff1de3799a44c2545a5da04d5ef9c7397fa
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-08-02 02:06:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from generate_mp3 import generate_mp3
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def generate():
     return jsonify({"message": result})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
